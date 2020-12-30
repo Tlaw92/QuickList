5c543a53663b202009fdcc9e6bac3fd0208d0aa7
Travis <travislawclimb@gmail.com>
2020-12-30 10:25:01 -0800

    implement roles into application.

--- QuickList/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ QuickList/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -11,33 +11,47 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Logging;
 
 namespace QuickList.Areas.Identity.Pages.Account
 {
-    [AllowAnonymous]
+    // 1* By utilizing Dependency Injection, we are going to inject the RoleManager class into the RegisterModel class via its constructor.
+    
+        [AllowAnonymous]
     public class RegisterModel : PageModel
     {
         private readonly SignInManager<IdentityUser> _signInManager;
         private readonly UserManager<IdentityUser> _userManager;
         private readonly ILogger<RegisterModel> _logger;
         private readonly IEmailSender _emailSender;
+        // 1* Add RoleManager
+        private readonly RoleManager<IdentityRole> _roleManager;
+
 
         public RegisterModel(
             UserManager<IdentityUser> userManager,
             SignInManager<IdentityUser> signInManager,
             ILogger<RegisterModel> logger,
-            IEmailSender emailSender)
+            IEmailSender emailSender,
+            // 1* Add RoleManager
+            RoleManager<IdentityRole> roleManager)
+
         {
             _userManager = userManager;
             _signInManager = signInManager;
             _logger = logger;
             _emailSender = emailSender;
+            // 1* Add RoleManager
+            _roleManager = roleManager;
+
         }
 
-        [BindProperty]
+    [BindProperty]
         public InputModel Input { get; set; }
+        // 3* Next, we need to add an additional property to the RegisterModel class. We will use this property to populate a drop-down list on our register html page so the user can choose what role to assign to their account.
+        public SelectList Roles { get; set; }
 
         public string ReturnUrl { get; set; }
 
@@ -60,12 +74,19 @@ namespace QuickList.Areas.Identity.Pages.Account
             [Display(Name = "Confirm password")]
             [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
             public string ConfirmPassword { get; set; }
+
+            //2* We are going to add a role property that we expect back from the view.That way we can edit our Register.cshtml view to include this new input and option
+            [Required]
+            public string Role { get; set; }
         }
 
         public async Task OnGetAsync(string returnUrl = null)
         {
             ReturnUrl = returnUrl;
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+            //4* Next, we need to edit the OnGetAsync method within the RegisterModel class to grab all of the roles in our database and initialize our new Roles property with some values for the drop-down list.
+            var roles = _roleManager.Roles;
+            Roles = new SelectList(roles, "Name", "Name");
         }
 
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
@@ -78,6 +99,11 @@ namespace QuickList.Areas.Identity.Pages.Account
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
+                    //5* Lastly, we need to edit the OnPostAsync method to add the new user role relationship to the database when a new user is successfully created.
+                    if (await _roleManager.RoleExistsAsync(Input.Role))
+                    {
+                        await _userManager.AddToRoleAsync(user, Input.Role);
+                    }
                     _logger.LogInformation("User created a new account with password.");
 
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
